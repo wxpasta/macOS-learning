34961ec027f53039a3fb4d6509d71a403b041d4a
许宝吉 <xubj@magictech.com.cn>
2020-12-31 13:40:48 +0800

    加入描述

--- Cocoa/NSTextField/TextField-XIB/TextFieldTest/Classes/MainViewController.m
+++ Cocoa/NSTextField/TextField-XIB/TextFieldTest/Classes/MainViewController.m
@@ -24,6 +24,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do view setup here.
+    
+    /*
+     NSTextField 是一个可以展示或编辑的文本框，是最常用的UI控件之一。
+     需要注意它与iOS的UITextFeild是有区别:
+     在iOS开发中，UILabel是用来展示固定的文本的，UITextField是用来编辑文本的。
+     在macOS中，它们两者是合二为一的，通过isEditable这个属性来决定展示的样式。
+     */
 }
 
 - (IBAction)onLoginClicked:(id)sender {
